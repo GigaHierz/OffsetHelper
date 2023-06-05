b3cb58004afc1e1d237a85ef623676f668144c7b
GigaHierz <lenahierzi@hotmail.de>
2023-06-06 00:11:14 +0100

    improvement: add Polygon address to OffsetStorage to test new on Polygon

--- contracts/OffsetHelperStorage.sol
+++ contracts/OffsetHelperStorage.sol
@@ -10,11 +10,11 @@ contract OffsetHelperStorage is OwnableUpgradeable {
     // token symbol => token address
     mapping(string => address) public eligibleTokenAddresses;
     address public contractRegistryAddress =
-        // 0x48E04110aa4691ec3E9493187e6e9A3dB613e6e4; // Alfajores
-        0xa30589F50b9641dacCB98AA2B4A8F24739c5B007; // Celo
+        // 0xa30589F50b9641dacCB98AA2B4A8F24739c5B007; // Celo
+        0x263fA1c180889b3a3f46330F32a4a23287E99FC9; //Polygon
     address public dexRouterAddress =
-        0x7D28570135A2B1930F331c507F65039D4937f66c; // Ubeswap
-    // 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506 // SushiSwap
+        // 0x7D28570135A2B1930F331c507F65039D4937f66c; // Ubeswap
+        0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506; // SushiSwap
     // user => (token => amount)
     mapping(address => mapping(address => uint256)) public balances;
     string public baseToken; // token that the exchange uses to swap to pool tokens e.g., NCTs
