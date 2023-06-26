713697684350b753ae6c02f1c06a0a77943f9b2e
GigaHierz <lenahierzi@hotmail.de>
2023-06-26 23:55:39 +0200

    add address choices for router

--- contracts/OffsetHelperStorage.sol
+++ contracts/OffsetHelperStorage.sol
@@ -16,10 +16,10 @@ contract OffsetHelperStorage is OwnableUpgradeable {
         // 0x48E04110aa4691ec3E9493187e6e9A3dB613e6e4; // Alfajores
         0xa30589F50b9641dacCB98AA2B4A8F24739c5B007; // Celo
     address public dexRouterAddress =
-        // 0x7D28570135A2B1930F331c507F65039D4937f66c; // Ubeswap
-        // 0xd9145CCE52D386f254917e481eB44e9943F39138; // Ubeswap - remix
-        // 0xa730B463395f5ca07EcE5cefeccF7f45e1E2C9Bf; // Ubeswap - sdk
-        0xE3D8bd6Aed4F159bc8000a9cD47CffDb95F96121; // Ubeswap - docs
+        0xa730B463395f5ca07EcE5cefeccF7f45e1E2C9Bf; // Ubeswap- from explorer with NCT
+    // 0xd9145CCE52D386f254917e481eB44e9943F39138; // Ubeswap - remix
+    // 0xa730B463395f5ca07EcE5cefeccF7f45e1E2C9Bf; // Ubeswap - sdk
+    // 0xE3D8bd6Aed4F159bc8000a9cD47CffDb95F96121; // Ubeswap - docs
     // 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506; // SushiSwap
 
     // user => (token => amount)
